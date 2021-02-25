1ff5e562fea92ceda42d7a88f7999f8c02e5f0cf
arianefranca <ariane_franca@sicredi.com>
2021-02-25 14:00:32 -0300

    ContractTest Updated

--- src/test/java/contract/ContractTest.java
+++ src/test/java/contract/ContractTest.java
@@ -1,7 +1,6 @@
 package contract;
 
 import org.testng.annotations.Test;
-
 import java.io.File;
 
 import static io.restassured.RestAssured.given;
@@ -11,17 +10,12 @@ public class ContractTest {
 
     @Test
     public void getBarrigaContractTest() {
-        //TODO
-        //COLOCAR OS VALORES DO HEADER NUM BASETEST OU EM UMA PATA DE MASSA DE DADOS
-        String headerValue = "JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MTM0ODJ9.gx7pnWtTgsOVPl8LRE9kYu5L158QnvRmXYs25eFZZMo";
-
         given().
                 baseUri("http://barrigarest.wcaquino.me/").
-                header("Authorization", headerValue).
+                header("Authorization", "JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MTM0ODJ9.gx7pnWtTgsOVPl8LRE9kYu5L158QnvRmXYs25eFZZMo").
         when().
                 get("saldo").
         then().
-            body(matchesJsonSchema(new File("scr/test/resources/jsonSchema/getCustomerBalanceJsonSchema.json")));
-
+            body(matchesJsonSchema(new File("src/test/resources/jsonSchema/getCustomerBalanceJsonSchema.json")));
     }
 }
