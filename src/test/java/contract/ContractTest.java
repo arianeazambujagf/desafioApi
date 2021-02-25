2d1858d9013ffc49ce31d87f28249b592e24bd43
arianefranca <ariane_franca@sicredi.com>
2021-02-25 15:25:48 -0300

    ContractTest with DataFactory

--- src/test/java/contract/ContractTest.java
+++ src/test/java/contract/ContractTest.java
@@ -2,17 +2,18 @@ package contract;
 
 import org.testng.annotations.Test;
 import java.io.File;
+import factory.DataFactory;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchema;
 
 public class ContractTest {
-
+    
     @Test
     public void getBarrigaContractTest() {
         given().
                 baseUri("http://barrigarest.wcaquino.me/").
-                header("Authorization", "JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MTM0ODJ9.gx7pnWtTgsOVPl8LRE9kYu5L158QnvRmXYs25eFZZMo").
+                header("Authorization", DataFactory.HeaderToken()).
         when().
                 get("saldo").
         then().
