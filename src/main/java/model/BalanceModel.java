a5387ad72a5eaf42624f6341b27170b0abcfbbad
arianefranca <ariane_franca@sicredi.com>
2021-02-26 14:00:28 -0300

    - BalanceTest

---
+++ src/main/java/model/BalanceModel.java
@@ -0,0 +1,21 @@
+package model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@RequiredArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+
+public class BalanceModel {
+
+    private int conta_id;
+    private String conta;
+    private String saldo;
+
+}
